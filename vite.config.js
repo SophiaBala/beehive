70ab7208bebaf87f925b485ef8bfeeb9351b61c1
SophiaBala <sofiabala06@gmail.com>
2026-01-12 22:47:37 +0200

    Link adapt

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const isProd = process.env.NODE_ENV === "production";
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isProd ? "/beehive/" : "/"  
+  base: "/beehive/"
 });
